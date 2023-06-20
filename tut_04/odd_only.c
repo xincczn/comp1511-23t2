ca3f9ad1bdb42936311b8a9691d12a7b46525850
Xing He <z5413977@ad.unsw.edu.au>
2023-06-20 17:32:11 +1000

    feat: implement odd_only

---
+++ tut_04/odd_only.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/*
+    Create an integer array with at least 5 elements.
+
+    Create a while loop which loops through every element of the array.
+
+    Write an if statement which adds 1 to each even value. Do this within the while loop.
+
+    Write another while loop which goes through the array with a different iterator (i.e. if you used i last time, use j)
+
+    Print out the values in the array.
+*/
+
+int main(void) {
+    // We declare arrays similar to normal data type
+    // int [] is the type here, but the tricky part is that the [] comes after the variable name
+    int array[5] = {1, 2, 3, 4, 5};
+    
+    // Here, `i` is our counter which checks each index of our array starting from 0 -> 4
+    // Remember that array indices start at 0 and not 1 so be careful
+    int i = 0;
+    while (i < 5) {  // Here we stop when `i` is equal to 5, which is the size of the array
+        // When we index our array `array[i]` we can now treat this as a variable holding a value
+        // And we can perform all the operations and actions likewise
+        // Using mod 2 allows us to check for even/odd numbers based on remainder
+        if (array[i] % 2 == 0) {
+            array[i]++;
+        }
+
+        i++;
+    }
+
+    // Using `j` for second loop, becareful of reusing same variable
+    // If you do, make sure to reset it to 0 (or whatever the starting value is)
+    int j = 0;
+    while (j < 5) {
+        printf("%d ", array[j]);
+        j++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
